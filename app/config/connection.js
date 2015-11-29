ec1a42641e6f847758cf8038159e2ad5378eeceb
Jarvis Badgley <chiper@chipersoft.com>
2015-11-28 20:28:43 -0800

    Switch to using a MySQL connection pool

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'hammer_test'
 });
 
-connection.connect();
-
-module.exports = connection;
+module.exports = pool;
